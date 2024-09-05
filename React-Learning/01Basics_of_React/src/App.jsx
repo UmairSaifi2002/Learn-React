15801ab138a0b2a1c0900708d9d3e0369a7c78b6
Umair Saifi <umairsaifi2002@gmail.com>
2024-09-06 00:16:21 +0530

    New Explanation in App.jsx

--- React-Learning/01Basics_of_React/src/App.jsx
+++ React-Learning/01Basics_of_React/src/App.jsx
@@ -13,3 +13,7 @@ function App() {
 }
 
 export default App
+
+// Now we Learn About how to create React Components
+// Create your file using extension if you are using vite ->  '.jsx'  else -> '.js'
+// either you are using vite or normal React use Capital Word to defining the function and use it to import or use in return statement
